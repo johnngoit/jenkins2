be865b0e3226e1a46a29e7d91a8b2e782f4f1500
John Ngo <johnngoit@gmail.com>
2018-08-17 21:32:26 -0700

    all

---
+++ SeleniumTest/SeleniumProject/SeleniumProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumProject")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("525c194d-9636-4d6b-b0c7-68d3a13908ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumTest/SeleniumProject/SeleniumProject/UnitTest1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.Edge;
+//using OpenQA.Selenium.PhantomJS;
+
+namespace SeleniumProject
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestChrome()
+		{
+			IWebDriver driver = null;
+			try
+			{
+				driver = new ChromeDriver(@"C:\tools");
+				driver.Url = "http://www.softpost.org";
+				driver.Manage().Window.Maximize();
+				driver.Navigate();
+			}
+			catch (Exception ex)
+			{
+				Console.Write("Exception..." + ex.ToString());
+			}
+			finally
+			{
+				Thread.Sleep(2000);
+				driver.Close();
+				driver.Quit();
+			}
+		}
+
+		[TestMethod]
+		public void TestIE()
+		{
+			IWebDriver driver = null;
+			//InternetExplorerOptions op = new InternetExplorerOptions();
+			try
+			{
+				driver = new InternetExplorerDriver(@"C:\tools");
+				driver.Url = "http://www.softpost.org";
+				driver.Manage().Window.Maximize();
+				driver.Navigate();
+			}
+			catch (Exception ex)
+			{
+				Console.Write("Exception..." + ex.ToString());
+			}
+			finally
+			{
+				Thread.Sleep(2000);
+				driver.Close();
+				driver.Quit();
+			}
+		}
+
+		[TestMethod]
+		public void TestFireFox()
+		{
+			IWebDriver driver = null;
+			FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\tools");
+			service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+			service.HideCommandPromptWindow = true;
+			service.SuppressInitialDiagnosticInformation = true;
+			driver = new FirefoxDriver(service);
+
+			try
+			{
+				//driver = new FirefoxDriver();
+				driver.Url = "http://www.softpost.org";
+				driver.Manage().Window.Maximize();
+				driver.Navigate();
+			}
+			catch (Exception ex)
+			{
+				Console.Write("Exception..." + ex.ToString());
+			}
+			finally
+			{
+				Thread.Sleep(2000);
+				driver.Close();
+				driver.Quit();
+			}
+		}
+	}
+}
